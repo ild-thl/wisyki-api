939cab17b7a888cf73eaa6dcada0f4c8c5d1fdba
Pascal Hürten <pascal.huerten@th-luebeck.de>
2023-05-27 11:45:10 +0200

    Move vectorstore back to data

--- vectorsearcher.py
+++ vectorsearcher.py
@@ -3,8 +3,7 @@ from langchain.vectorstores import Chroma
 
 class vectorsearcher():
     def __init__(self, embedding):
-        persist_directory = 'esco_vectorstore'
-        
+        persist_directory = 'data/esco_vectorstore'
         self.vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
 
     def predict(self, doc, top_k):
